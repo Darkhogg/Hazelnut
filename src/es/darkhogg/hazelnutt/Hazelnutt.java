e55ad1ad07ed2bc75981677c4c06d67744095ddd
Daniel Escoz <darkhogg@gmail.com>
2011-08-02 00:43:20 +0200

    Updated to version 1.0.8_1 (forgot in las commit)

--- src/es/darkhogg/hazelnutt/Hazelnutt.java
+++ src/es/darkhogg/hazelnutt/Hazelnutt.java
@@ -43,7 +43,7 @@ public final class Hazelnutt {
 	/**
 	 * Version of the program
 	 */
-	private static final Version VERSION = new Version( 1, 0, 8, 0 );
+	private static final Version VERSION = new Version( 1, 0, 8, 1 );
 	
 	/**
 	 * Logger for the whole application
